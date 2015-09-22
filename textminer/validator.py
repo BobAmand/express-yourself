a91f9c8f2b8ad044fb04fdde610e8551d888d8b8
Bob Amand <biotmg@nc.rr.com>
2015-09-22 17:54:44 -0400

    Draft 22-Sep-15 5:50pm

---
+++ textminer/validator.py
@@ -0,0 +1,14 @@
+import pytest
+
+'''
+The Iron Works - Exercise 14 express-yourself
+- Objective is to test 'regular expressions'
+- Three test areas have been established:
+   * extractor
+   * separator
+   * validator  [current file]
+
+'''
+def binary_numbers():
+    binary_set = re.compile("xxxx"))
+    if 
